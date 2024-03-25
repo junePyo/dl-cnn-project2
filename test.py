c1ada337dffe6acf6bd7d73169a21a825fc0cd25
nuclear_pasta02 <malini2700@gmail.com>
2024-03-25 16:34:13 -0400

    created testing file
    this file only uses code from the project description

---
+++ test.py
@@ -0,0 +1,26 @@
+import cv2
+import os
+import glob
+
+img_dir = 'face_images'
+files = glob.glob(os.path.join(img_dir, '*.jpg'))
+
+image_ = cv2.imread(files[0])
+imageLAB = cv2.cvtColor(image_, cv2.COLOR_BGR2LAB)
+L_, a_, b_=cv2.split(imageLAB)
+
+cv2.imshow('image', image_)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imshow('imageLAB', imageLAB)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imshow('L',L_)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imshow('a',a_)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imshow('b',b_)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
